4a22b61edb6df83e55ab22e0e883c45955b625e1
Sid <gowda.9@osu.edu>
2014-11-16 01:38:42 -0500

    Created GeneticAlgorithm SudokuSolver class and updated TestHarness

---
+++ CSE3521Lab3/src/SudokuSolverGA.java
@@ -0,0 +1,27 @@
+import java.util.Vector;
+
+
+public class SudokuSolverGA {
+
+	
+	/**
+	 * Create the initial population
+	 */
+	public SudokuSolverGA(Vector<Integer> board)
+	{
+		
+	}
+	
+	
+	public Vector<Integer> geneticAlgorithm()
+	{
+		return null;
+	}
+	
+	public void fitnessFunction()
+	{
+		
+	}
+	
+	
+}
--- CSE3521Lab3/src/TestHarness.java
+++ CSE3521Lab3/src/TestHarness.java
@@ -26,15 +26,15 @@ public class TestHarness {
 	public static void main(String[] args) {
 		
 		Sudoku board = null;
-		SudokuSolver agent;
-		Vector<Integer> solState;
+		SudokuSolver agentHill;
+		SudokuSolverGA agentGA;
+		Vector<Integer> solStateHillClimber, solStateGeneticAlg;
 		
 		if(args.length != 2)
 		{
 			System.err.println("Too little arguments. Need an input file.");
 			System.exit(1);//exit the program
 		}
-		
 		try
 		{
 			/*
@@ -48,26 +48,54 @@ public class TestHarness {
 			System.exit(1);
 		}
 		
-		//create SudokuSolver object and
-		//call hill Climber function and it will return the solution state
-		agent = new SudokuSolver(board.getIndexesOfInitialBoard());
-		solState = agent.hillClimber(board.getSudokuBoard());
+		//create SudokuSolver object and SudukoSolverGA object
+		//call hill Climber function and geneticAlgorithm and they will return the solution state
+		agentHill = new SudokuSolver(board.getIndexesOfInitialBoard());
+		agentGA = new SudokuSolverGA(board.getSudokuBoard());
 		
-		//print out the solution state
-		for(int i = 0; i < solState.size();i++)
+		System.out.println("Sudoku problem solved using the Hill Climber Algorithm");
+		solStateHillClimber = agentHill.hillClimber(board.getSudokuBoard());
+		
+		//print out the solution state of the hill climber
+		System.out.println("Solution State");
+		for(int i = 0; i < solStateHillClimber.size();i++)
 		{
 			if(i % 4 == 0 && i!=0)
 			{
 				System.out.print("|\n");
-				System.out.print("|"+solState.get(i));
+				System.out.print("|"+solStateHillClimber.get(i));
 			}
 			else if(i == 15)
 			{
-				System.out.println("|"+solState.get(i)+"|");
+				System.out.println("|"+solStateHillClimber.get(i)+"|");
 			}
 			else
 			{
-				System.out.print("|"+solState.get(i));
+				System.out.print("|"+solStateHillClimber.get(i));
+			}
+		}
+		
+		
+		//now do the genetic algorithm
+		
+		System.out.println("Sudoku problem solved using the Genetic Algorithm");
+		solStateGeneticAlg = agentGA.geneticAlgorithm();
+		//print out the solution state of the genetic algorithm
+		System.out.println("Solution State");
+		for(int i = 0; i < solStateGeneticAlg.size(); i++)
+		{
+			if(i % 4 == 0 && i!=0)
+			{
+				System.out.print("|\n");
+				System.out.print("|"+solStateGeneticAlg.get(i));
+			}
+			else if(i == 15)
+			{
+				System.out.println("|"+solStateGeneticAlg.get(i)+"|");
+			}
+			else
+			{
+				System.out.print("|"+solStateGeneticAlg.get(i));
 			}
 		}
 		
